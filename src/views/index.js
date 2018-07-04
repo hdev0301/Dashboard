334ba63d02c6ce695ee8395dfd3e8b6d3578b1e4
devac1103 <hdev0301@gmail.com>
2018-07-04 12:36:30 -0700

    Modified views indexing

--- src/views/index.js
+++ src/views/index.js
@@ -1,19 +1,19 @@
-import {
-  ManageArea,
-  ManageCategories,
-  ManageExercise,
-  ManageSubType,
-  ManageType,
-  MyFavorites,
-  ViewExercises,
-} from 'Library';
+import DashboardHome from './DashboardHome';
+import Feedback from './Feedback';
+import HelpAndSupport from './HelpAndSupport';
+import Library from './Library';
+import Patients from './Patients';
+import SystemManagement from './SystemManagement';
+import Therapists from './Therapists';
+import Training from './Training';
 
 export {
-  ManageArea,
-  ManageCategories,
-  ManageExercise,
-  ManageSubType,
-  ManageType,
-  MyFavorites,
-  ViewExercises,
+  DashboardHome,
+  Feedback,
+  HelpAndSupport,
+  Library,
+  Patients,
+  SystemManagement,
+  Therapists,
+  Training,
 };
