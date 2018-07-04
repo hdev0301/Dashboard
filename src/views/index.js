115f4aa6051c3b8ca1f43e5384b6117e65e1a40f
devac1103 <hdev0301@gmail.com>
2018-07-04 11:16:13 -0700

    Added views indexing

---
+++ src/views/index.js
@@ -0,0 +1,19 @@
+import {
+  ManageArea,
+  ManageCategories,
+  ManageExercise,
+  ManageSubType,
+  ManageType,
+  MyFavorites,
+  ViewExercises,
+} from 'Library';
+
+export {
+  ManageArea,
+  ManageCategories,
+  ManageExercise,
+  ManageSubType,
+  ManageType,
+  MyFavorites,
+  ViewExercises,
+};
