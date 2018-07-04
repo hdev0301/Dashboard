7a4650fffccc059137cd01a438f010e54d1d47f1
devac1103 <hdev0301@gmail.com>
2018-07-04 11:12:27 -0700

    Added Library view indexing

---
+++ src/views/Library/index.js
@@ -0,0 +1,17 @@
+import ManageArea from './ManageArea';
+import ManageCategories from './ManageCategories';
+import ManageExercise from './ManageExercise';
+import ManageSubType from './ManageSubType';
+import ManageType from './ManageType';
+import MyFavorites from './MyFavorites';
+import ViewExercises from './ViewExercises';
+
+export {
+  ManageArea,
+  ManageCategories,
+  ManageExercise,
+  ManageSubType,
+  ManageType,
+  MyFavorites,
+  ViewExercises,
+};
